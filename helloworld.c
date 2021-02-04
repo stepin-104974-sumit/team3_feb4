2cc2b454c62af0f82c18db8dc56b14c6d7004f54
stepin104865 <69194460+stepin104865@users.noreply.github.com>
2021-02-04 15:26:23 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
+Use Git or checkout with SVN using the web URL. 
 {
     printf("Hello world, this is sumit");
     printf("Hello world, this is christy");
     printf("Hello world, this is Dhyan");
-    printf("hello world, this is satyasindhu");
     return 0;
 }
